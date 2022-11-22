e7af185dd4a92e2dd102ed9c62f6cbb40eb4cae3
HugoVeraGarcia <hugo.vera.garcia@gmail.com>
2022-11-22 10:21:07 -0500

    first commit

---
+++ database/shipping.model.js
@@ -0,0 +1,70 @@
+const { DataTypes } = require('sequelize');
+const { db } = require('../utils/database');
+
+const Shipping = db.define('shipping', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+    unique: true,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  ubigeo_id_origin: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  destiny_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  destiny_address: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  ubigeo_id_destiny: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  branch_id: {
+    type: DataTypes.INTEGER,
+  },
+  shipping_date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  high_size: {
+    type: DataTypes.INTEGER,
+  },
+  width_size: {
+    type: DataTypes.INTEGER,
+  },
+  large_size: {
+    type: DataTypes.INTEGER,
+  },
+  weight: {
+    type: DataTypes.FLOAT,
+    defaultValue: 'active',
+  },
+  special_cares: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  status: {
+    type: DataTypes.STRING,
+    defaultValue: 'active',
+  },
+});
+
+module.exports = { Shipping };
